059caca6d3d752c732fd498d420a7f41055f6914
avelreserch <pavel.oliynyk@gmail.com>
2020-08-27 10:20:05 +0100

    Add set of typed actors as example

--- build.sbt
+++ build.sbt
@@ -7,3 +7,5 @@ scalaVersion := "2.13.3"
 val AkkaVersion = "2.6.8"
 
 libraryDependencies += "com.typesafe.akka" %% "akka-actor-typed" % AkkaVersion
+
+libraryDependencies += "ch.qos.logback" % "logback-classic" % "1.2.3"
---
+++ src/main/scala/HelloWorldMain.scala
@@ -0,0 +1,27 @@
+import actors.{HelloWorld, HelloWorldBot}
+import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.{ActorSystem, Behavior}
+
+object HelloWorldMain {
+
+  final case class SayHello(name: String)
+
+  def apply(): Behavior[SayHello] =
+    Behaviors.setup { context =>
+      val greeter = context.spawn(HelloWorld(), "greeter")
+
+      Behaviors.receiveMessage { message =>
+        val replyTo = context.spawn(HelloWorldBot(max = 3), message.name)
+        greeter ! HelloWorld.Greet(message.name, replyTo)
+        Behaviors.same
+      }
+    }
+
+  def main(args: Array[String]): Unit = {
+    val system: ActorSystem[HelloWorldMain.SayHello] =
+      ActorSystem(HelloWorldMain(), "hello")
+
+    system ! HelloWorldMain.SayHello("World")
+    system ! HelloWorldMain.SayHello("Akka")
+  }
+}
